c5faea0db0467d62814fd73eb90b8abcdf0564c7
Wasim2708 <101546593+Wasim2708@users.noreply.github.com>
2024-05-18 21:50:00 +0530

    Update platformKit target

--- Package.swift
+++ Package.swift
@@ -3,8 +3,6 @@
 
 import PackageDescription
 
-import PackageDescription
-
 let package = Package(
     name: "ZohoDeskPortalConfiguration",
     products: [
@@ -19,9 +17,13 @@ let package = Package(
         .target(name: "ZohoDeskPortalConfigurationTargets",
                 dependencies: [
                     .target(name: "ZohoDeskPortalAPIKit", condition: .when(platforms: [.iOS])),
-                    .target(name: "ZohoDeskPortalConfiguration", condition: .when(platforms: [.iOS]))
+                    .target(name: "ZohoDeskPortalConfiguration", condition: .when(platforms: [.iOS])),
+                    .target(name: "ZDPDataBridge", condition: .when(platforms: [.iOS])),
+                    .target(name: "ZDPUIBuilder", condition: .when(platforms: [.iOS]))
                 ]),
         .binaryTarget(name: "ZohoDeskPortalAPIKit", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalAPIKit/3.0.9/ZohoDeskPortalAPIKit.zip", checksum: "e71aa0a89ae397e43b47d67d1c2eca59ca2fc2f9fdd1a0d84422408bc73b4ac1"),
-        .binaryTarget(name: "ZohoDeskPortalConfiguration", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalConfiguration/3.0.9/ZohoDeskPortalConfiguration.zip", checksum: "7493243c48bb88b05dc885f85778d57a40be60d58a71c0cbb1bd7e3b8dca8059")
+        .binaryTarget(name: "ZohoDeskPortalConfiguration", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalConfiguration/3.0.9/ZohoDeskPortalConfiguration.zip", checksum: "7493243c48bb88b05dc885f85778d57a40be60d58a71c0cbb1bd7e3b8dca8059"),
+        .binaryTarget(name: "ZDPDataBridge", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPlatformDataBridge/iOS/1.0.15/ZDPDataBridge.zip", checksum: "e65e32bb7ffa5764a3558a8004f1d030acce55d730e3ec1e8651b628f9b4e2f0"),
+        .binaryTarget(name: "ZDPUIBuilder", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPlatformUIKit/iOS/1.0.15/ZDPUIBuilder.zip", checksum: "fae35ba49a6b5780cfa190f4a2b3939a5996c3c66e4829a69a690899daa93581")
     ]
 )
