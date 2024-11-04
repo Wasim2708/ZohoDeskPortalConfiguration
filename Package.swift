36cc1b93f83f8a360d7d1500759793996d165345
Wasim2708 <101546593+Wasim2708@users.noreply.github.com>
2024-11-04 10:42:58 +0530

    Update Package.swift to 4.0.1

--- Package.swift
+++ Package.swift
@@ -6,13 +6,21 @@ let package = Package(
     products: [
         .library(
             name: "ZohoDeskPortalConfiguration",
-            targets: ["ZohoDeskPortalConfiguration", "ZohoDeskPortalAPIKit", "ZDMediaPickerSDK", "ZohoDeskPlatformDataBridge", "ZohoDeskPlatformUIKit"])
+            targets: ["ZohoDeskPortalConfiguration", "ZohoDeskPortalConfigurationPackage"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/LakshmiChidambaranathan/ZohoDeskPlatformUIKit.git", branch: "main"),
+        .package(url: "https://github.com/Wasim2708/ZohoDeskPortalAPIKit.git", exact: "4.0.1")
     ],
     targets: [
         .binaryTarget(name: "ZohoDeskPortalConfiguration", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalConfiguration/4.0.1/ZohoDeskPortalConfiguration.zip", checksum: "3090843a8ee8013a986fdaa9528ecc0eca7c4c189a54b3b8e28ca9cf42bb1a4d"),
-        .binaryTarget(name: "ZohoDeskPortalAPIKit", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPortalAPIKit/4.0.1/ZohoDeskPortalAPIKit.zip", checksum: "5ba45cf0a22f5c3e4279ff87a580ed89f413e8001cbfe354e321c2cb98952650"),
-        .binaryTarget(name: "ZDMediaPickerSDK", url: "https://maven.zohodl.com/ZohoDesk/ZDMediaPickerSDK/iOS/1.0.8/XCFramework.zip", checksum: "2cbe82433ca5ecad2f993d2d45aa99e0e9f7e18a3175cff31178d671f13d3067"),
-        .binaryTarget(name: "ZohoDeskPlatformDataBridge", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPlatformDataBridge/iOS/2.0.0-beta.9/ZDPDataBridge.zip", checksum: "70d5c980cc032d060b57b8852dae3bb9341ca8d25b6cbd224ccef5016e623305"),
-        .binaryTarget(name: "ZohoDeskPlatformUIKit", url: "https://maven.zohodl.com/ZohoDesk/ZohoDeskPlatformUIKit/iOS/2.0.0-beta.9/ZDPUIBuilder.zip", checksum: "9b4eb139697d5b54f5a8a41cbba4dc3317ee80f6f0a9f15564e4d85954ec4420")
+        .target(
+            name: "ZohoDeskPortalConfigurationPackage",
+            dependencies: [
+                .product(name: "ZohoDeskPlatformUIKit", package: "ZohoDeskPlatformUIKit"),
+                .product(name: "ZohoDeskPortalAPIKit", package: "ZohoDeskPortalAPIKit")
+            ]
+        ),
+
     ]
 )
